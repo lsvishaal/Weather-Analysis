12fd57609e3283722426572d8d64a1854ab0e2ce
Vishaal L.S <62366204+lsvishaal@users.noreply.github.com>
2024-03-27 00:15:30 +0530

    90% complete, created routes and chart data and sent to meteorological-factors.html , have to connect temperature and meteo pages with equal domain naming

--- app.py
+++ app.py
@@ -39,6 +39,18 @@ def fetch_apparent_temp_data(city, connection):
         return cursor.fetchall()
 
 
+# Function to fetch meteorological factors data
+def fetch_meteorological_factors_data(city, connection):
+    with connection.cursor() as cursor:
+        if city.lower() not in ["chennai", "madurai"]:
+            raise ValueError(f"Invalid city: {city}")
+
+        cursor.execute(
+            f"SELECT time, surface_pressure, relative_humidity_2m as relative_humidity, wind_speed_10m as wind_speed FROM {city.lower()}_weather WHERE HOUR(time) = 9"
+        )
+        return cursor.fetchall()
+
+
 @app.route("/api/weather-data/<city>")
 def weather_data(city):
     connection = get_connection()
@@ -80,13 +92,31 @@ def apparent_weather_data(city):
     return jsonify(chart_data)
 
 
+@app.route("/api/meteorological-factors-data/<city>")
+def meteorological_factors_data(city):
+    connection = get_connection()
+    city_data = fetch_meteorological_factors_data(city, connection)
+    connection.close()
+
+    # Extract labels and meteorological factors from city_data
+    labels = [data["time"].strftime("%Y-%m-%d %H:%M:%S") for data in city_data]
+    surface_pressures = [data["surface_pressure"] for data in city_data]
+    relative_humidities = [data["relative_humidity"] for data in city_data]
+    wind_speeds = [data["wind_speed"] for data in city_data]
+
+    chart_data = {
+        "labels": labels,
+        "surfacePressures": surface_pressures,
+        "relativeHumidities": relative_humidities,
+        "windSpeeds": wind_speeds,
+    }
+
+    return jsonify(chart_data)
+
+
 @app.route("/meteorological-factors")
 def meteorological_factors_chart():
-    return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("meteorological-factors.html")
 
 
 if __name__ == "__main__":
